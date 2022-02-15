1faf021636f38d916130b34b767216b80c76cc3f
gabriel falk <gabrielfalk03@gmail.com>
2022-02-15 12:39:24 +0100

    Justering vid slutet av lektionen

--- index.php
+++ index.php
@@ -9,9 +9,45 @@
 <body>
     <?php
     session_start();
-    echo 'You are logged in as: ' . '<b>' . $_SESSION['loginname'] . '<b>';
-    echo '<h1>Welcome</h1>'
+    
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $DBname = "forum";
+    $conn = new mysqli($servername, $username, $password, $DBname);
+    $sql = "SELECT * FROM users (fname, lname, username, pass, pfp, time)";
 
+    /*
+    $fname = $_POST["fname"];
+    $lname = $_SESSION["lname"];
+    $username = $_SESSION["username"];
+    $pfp = $_SESSION["pfp"];
+    */
+
+    echo 'You are logged in as: ' . '<b>' . $_SESSION['loginname'] . "<br>" .  "Username: " . $_SESSION["username"] . '<b>';
+    echo '<h1>Welcome</h1>';
+ 
+
+    echo "<br> <br>" . '<form method="post" action="??????HÄR SKA DET VARA">' . '<button type="submit">' . //Få denna knapp att köra linje 35 - 43.
+     "Check if you are logged in" . '</button>' . '<form>' . "<br>";
+    echo "<a href='uploadFile.php'>Upload a file</a>";
+
+    if (isset($_SESSION["username"]) &&
+    !empty($_SESSION["username"])) {
+        echo "Du är inloggad som " . 
+        $_SESSION["username"];
+        echo '<form method="post" action="logoutPage.php">' . '<button type="submit">' . "Logout" . '</button>' . '<form>';
+    }
+    else{
+        echo "Du är inte inloggad.";
+    }
+
+    echo "<p class=p_acc>Your account details:</p>" . "Name: " . "<br>" . "Email: " . "<br>" . "Website: " . "<br>" . "Comment: " . "<br><br>";
+
+    echo "<br> <br>" . "Session started";
+    echo "<br> <br>" . '<form method="post" action="checklogin.php">' . '<button type="submit">' .
+     "Check if you are logged in" . '</button>' . '<form>' . "<br>";
+    echo "<a href='uploadFile.php'>Upload a file</a>";
 
 
 
