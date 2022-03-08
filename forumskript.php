cd250fff8bd5e229af0e13adc6fda99b94a6e985
BuildTools <unconfigured@null.spigotmc.org>
2022-03-08 12:09:33 +0100

    Fixade inloggningsknappen samt tog bort onödig kod

--- forumskript.php
+++ forumskript.php
@@ -7,7 +7,7 @@ session_start();
 
 // Inloggning  ----------------------------------------------------------------------------------------------------------------
 
-if ($_SESSION["Value"] == "Login") {
+if ($_SESSION["Value"] == "login") {
 
 $servername = "localhost";
 $username = "root";
