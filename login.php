e19b3899d8b7daa5cc991919cd43f97101916ad1
gabriel falk <gabrielfalk03@gmail.com>
2022-03-08 12:43:11 +0100

    Senaste från 8 mars

--- login.php
+++ login.php
@@ -8,39 +8,86 @@
 </head>
 <body>
 
+<?php
+session_start();
 
-    <form action="forumskript.php" method="post">
-        Username:<br>
-        <input type="text" name="username">
+if (isset($_POST['login'])) { // If login button is pressed
+  $_SESSION["Value"] = "Login";
+
+  echo '<form action="forumskript.php" method="post">
+          Username:<br>
+          <input type="text" name="username">
+          <br><br>
+          Password:<br>
+          <input type="password" name="password">
+          <br><br>
+          <input type="submit" value="Submit">
+        </form>
         <br><br>
-        Password:<br>
-        <input type="password" name="password">
-        <br><br>
-        <input type="submit" value="Submit">
-      </form>
-      <br><br>
-      <form action="signup.php">
-        <input type="submit" value="Sign up">
-    </form>
+        <form method="post">
+        <input type="submit" id="signup" name="signup" value="Signup">
+      </form>';
 
+  if (isset($_SESSION["Failed"]) && $_SESSION["Failed"] == "true"){
 
-      <?php
-      session_start();
-      $_SESSION["Value"] = "Login";
-      
-      if (isset($_SESSION["Failed"]) && $_SESSION["Failed"] == "true"){
+    echo "<h3 style='color:red'>Wrong username or password!</h3>";
+  
+  } else {
 
-          echo "<h3 style='color:red'>Wrong username or password!</h3>";
+    return;
+  
+  }
 
-      } else {
+} else {
+  $_SESSION["Value"] = "Signup";
+ 
+  echo '    <form action="forumskript.php" method="post" enctype="multipart/form-data">
+First Name:<br>
+<input type="text" name="fname">
+<br><br>
+Last Name:<br>
+<input type="text" name="lname">
+<br><br>
+Username: <br>
+<input type="text" name="username">
+<br><br>
+Password:<br>
+<input type="password" name="password">
+<br><br>
+Profile Picture:
+<input type="file" name="fileToUpload" value="Select file" id="fileToUpload">
+<input type="submit" value="Submit">
+</form><br>';
+  echo '<form method="post">
+  <input type="submit" id="login" name="login" value="Login">
+</form>';
+  if (isset($_SESSION["Failed"])){
 
-          return;
+    if ($_SESSION["Failed"] == "falsecheck"){
+
+        echo "<h3 style='color:red'>File is not an image</h3>";
+
+    } else if ($_SESSION["Failed"] == "size"){
+
+        echo "<h3 style='color:red'>Your file is too large</h3>";
         
-        }
+    } else if ($_SESSION["Failed"] == "format"){
 
-        $_SESSION["Failed"] = "false";
-      
-      ?>
+        echo "<h3 style='color:red'>Only JPG, JPEG, PNG & GIF files are allowed</h3>";
+        
+    } else {
+
+        return;
+
+    }
+  }
+}
+
+
+
+  $_SESSION["Failed"] = "false";
+
+?>
 
 
     
--- openthread.php
+++ openthread.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Open thread</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
@@ -25,87 +26,24 @@
 <!--===============================================================================================-->
 
     <link rel="stylesheet" href="css/style.css">
-    <title>Forum</title>
 </head>
 <body>
+<h5> Open thread page. Page for reading a specific chosen thread from "previousthreads.php"</h5> <br><br>
 
-    <?php
-    session_start();
-    
-    $servername = "localhost";
+<a name="" id="" class="btn btn-primary" href="previousthreads.php" role="button"><- Go back</a>
+
+<br><br><br> 
+
+
+<?php
+session_start();
+$servername = "localhost";
     $username = "root";
     $password = "";
     $DBname = "forum";
     $conn = new mysqli($servername, $username, $password, $DBname);
 
-   
-    echo '<h1>Welcome</h1>';
-    echo 'You are logged in as: ' . '<b>' . $_SESSION['loginname'] . "</b> <br>" .  "Username: <b>" . $_SESSION['username'] . '</b><br><br>';
-
-    ?>
-
-<!-- <script type="text/javascript">
-    function showForm() {
-        document.getElementById('formElement').style.display = 'block';
-        }
-</script>
-    -->
-
-<script type="text/javascript">
-    function myFunction() {
-  var x = document.getElementById("formElement");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-} 
-</script>
-
-<button type="button" onclick=myFunction() class="btn btn-outline-info btn-rounded" data-mdb-ripple-color="dark">Create new thread/post</button>
-
-<div id="formElement" class="bg-contact2" style="background-image: url('images/bg-01.jpg');">
-		<div class="container-contact2">
-			<div class="wrap-contact2">
-				<form class="contact2-form validate-form" action="createThread.php" method="post" name="formClass" onsubmit="return validateForm()">
-					<span class="contact2-form-title">
-						Create a Post
-					</span>
-
-					<div class="wrap-input2 validate-input" data-validate="Title is required">
-						<input class="input2" type="text" name="title" id="titleId">
-						<span class="focus-input2" data-placeholder="TITLE"></span>
-					</div>
-
-					<div class="wrap-input2 validate-input" data-validate = "Valid email is required, i.e. abc@123.com">
-						<input class="input2" type="email" name="contactInfo" id="contactId">
-						<span class="focus-input2" data-placeholder="EMAIL"></span>
-					</div>
-
-					<div class="wrap-input2 validate-input" data-validate = "Description is required">
-						<textarea class="input2" name="descr" id="descrId"></textarea>
-						<span class="focus-input2" data-placeholder="DESCRIPTION"></span>
-					</div>
-
-					<div class="wrap-input2">
-                        <input class="form-control form-control-sm" id="formFileSm" type="file" name="uploadedFile" placeholder="File">
-					</div>
-
-					<div class="container-contact2-form-btn">
-						<div class="wrap-contact2-form-btn">
-							<div class="contact2-form-bgbtn"></div>
-							<button class="contact2-form-btn">
-								Create Post
-							</button>
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-
-  <!-- Start på tidigare skapade trådar -->  
-  <a name="" id="" class="btn btn-primary" href="previousthreads.php" role="button">Previous threads</a>
+?>
 
 
 <!--===============================================================================================-->
@@ -132,7 +70,6 @@
 	  gtag('config', 'UA-23581568-13');
 	</script>
 -->
-
 
 
 </body>
